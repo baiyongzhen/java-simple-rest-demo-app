9c92487cb45b7fb68720337e2d308968fa1e36f1
Kari Marttila <kari.marttila@tieto.com>
2019-04-09 18:45:08 +0300

    Renated buildspec.yml (to buildspec_build_and_test.yml) since creating another buildspec file for creating Docker image

--- run-local-codebuild.sh
+++ run-local-codebuild.sh
@@ -2,5 +2,5 @@
 
 # See instructions in https://github.com/tieto-pc/aws-devops-intro-demo/blob/master/README.md#local-codebuild
 
-./codebuild_build.sh -i aws/codebuild/ubuntu:18 -a local-artifacts -b buildspec.yml
+./codebuild_build.sh -i aws/codebuild/ubuntu:18 -a local-artifacts -b buildspec_build_and_test.yml
 
